15241ee39e64eed17c16269227aafc6f4de8abd3
beommaker <beom.maker@gmail.com>
2017-11-17 19:17:24 +0900

    [modify] to better code
    AJAX, promise, async, await!
    
    https://app.asana.com/0/477221076931319/477221076931335

--- src/App.js
+++ src/App.js
@@ -2,49 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie'
 
-
 class App extends Component {
-
   // Render: componentWillMount() -> render() -> componentDidMount()
   // Update comonentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
   
-  state = {
-  }
+  state ={}
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            poster: "http://www.freemovieposters.net/posters/matrix_the_1999_3131_poster.jpg"
-          },
-          {
-            title: "Full Metal Jacket",
-            poster: "https://thesouloftheplot.files.wordpress.com/2014/06/poster_fullmetaljacket.jpg"
-          },
-          {
-            title: "Oldboy",
-            poster: "https://vignette.wikia.nocookie.net/cinemorgue/images/a/a2/OldBoy.jpg/revision/latest?cb=20141113025503"   
-          },
-          {
-            title: "Star Wars",
-            poster: "http://schmoesknow.com/wp-content/uploads/2017/10/POSTER1.jpg" 
-          },
-          {
-            title: "Transportting",
-            poster: "http://newsnet.scot/wp-content/uploads/2017/02/Trainspotting.jpg" 
-          },       
-        ]
-      })
-    }, 2000)
+   this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map((movie) => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/>
     })
     return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.ag/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
   }
 
   render() {
